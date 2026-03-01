1ce7ac4b3fe77d4fd9a1b704600d5f13c1c02cc4
kateget <1397945742@qq.com>
2026-03-01 12:42:28 +0800

    流光按钮添加

--- frontend/my-app/src/pages/cssAnimations/index.tsx
+++ frontend/my-app/src/pages/cssAnimations/index.tsx
@@ -12,6 +12,10 @@ const App: react.FC = () => {
             <div className='color-reverse'>
                 <span>颜色反转效果</span>
             </div>
+            {/* 流光按钮效果 */}
+            <div className='flowing-light-button'>
+                <span>流光按钮效果</span>
+            </div>
         </div>
     );
 };
